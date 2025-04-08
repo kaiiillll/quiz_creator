201e69e8f7b32d6b51f8106618ca415247e76462
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-08 11:34:19 +0800

    pseudocode

---
+++ quiz_creator.py
@@ -0,0 +1,7 @@
+# use def function 
+# print all following instructions
+# use the while true loop
+# if statement
+# break to stop the while true loop
+# save the file 
+# save the name of user then continue to the loop
