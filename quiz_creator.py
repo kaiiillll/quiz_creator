2f2732e1246967171db760c1dfda1b6b08bd5931
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-04-08 12:05:33 +0800

    print

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,5 +1,7 @@
 # use def function 
 # print all following instructions
+print("Quiz Creator")
+print("Enter your desired questions and the answers. Type 'EXIT' to stop the program.\n")
 # use the while true loop
 # if statement
 # break to stop the while true loop
