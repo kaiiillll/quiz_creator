2a752dc4b0d04bc2ffd963418c2c1b00b61e9812
Kyle Delos Santos <kyledelossantos@Kais-MacBook-Air.local>
2025-05-03 01:21:37 +0800

    needed_class_design_and_info_from_user

--- quiz_runner.py
+++ quiz_runner.py
@@ -5,5 +5,20 @@ import random
 import time
 from collections import defaultdict
 
-###
+# the class for brain roulette and design
+class BrainRouletteGame:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Brain Roulette Game")
+        self.root.geometry("800x600")
+        self.root.configure(bg="#000000")
+        
+# needed info from the user to know the person
+        self.player = {
+            "name": "",
+            "gender": "",
+            "age": 0,
+            "section": "",
+            "year": ""
+        }
 
